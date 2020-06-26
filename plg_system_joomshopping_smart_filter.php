56088c76f8367db6c408bd20e02f1f8e02469a3b
oleg <sad.net79@gmail.com>
2020-06-26 14:28:56 +0300

    plg_system_joomshopping_smart_filter version 1.3

--- plg_system_joomshopping_smart_filter.php
+++ plg_system_joomshopping_smart_filter.php
@@ -13,6 +13,8 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Plugin\CMSPlugin;
 
+	JLoader::registerNamespace('GNZ11',JPATH_LIBRARIES.'/GNZ11',$reset=false,$prepend=false,$type='psr4');
+
 /**
  * Plg_system_joomshopping_smart_filter plugin.
  *
@@ -90,8 +92,91 @@ class plgSystemPlg_system_joomshopping_smart_filter extends CMSPlugin
 	 */
 	public function onAfterRender()
 	{
-		// Access to plugin parameters
-		$sample = $this->params->get('sample', '42');
+
+
+		$body = $this->app->getBody();
+		$dom = new \GNZ11\Document\Dom();
+
+		$dom->loadHTML( mb_convert_encoding( $body , 'HTML-ENTITIES', 'UTF-8' ) );
+		$xpath = new \DOMXPath( $dom );
+		$Nodes = $xpath->query( '//div[contains(@class , "uf_input")]/label' );
+
+		$new_A = $dom->createElement('a');
+		$new_A->setAttribute('class','wrapper');
+		$new_A->setAttribute('','wrapper');
+
+
+
+
+		$uri = \Joomla\CMS\Uri\Uri::getInstance();
+		$characteristics_Var = $uri->getVar('characteristics') ;
+		
+
+
+		
+		foreach( $Nodes as $Ni => $node )
+		{
+			$uri = clone $uri ;
+
+			$inputElements = $node->parentNode->getElementsByTagName('input') ;
+			$labelElements = $node->parentNode->getElementsByTagName('label') ;
+
+			foreach( $inputElements as $inputElement )
+			{
+				$name = $inputElement->getAttribute( 'name' ) ;
+				$value = $inputElement->getAttribute( 'value' ) ;
+			}#END FOREACH
+			if( !$name )
+			{
+				$name = null ;
+			}#END IF
+			switch($name){
+				case 'manufacturers[]' :
+					$uri_Clone = clone $uri ;
+					$manufacturers = $uri->getVar('manufacturers') ;
+					$manufacturers[] = $value ;
+					$uri_Clone->setVar('manufacturers' , $manufacturers ) ;
+					# Создаем Ссылку
+					$currentUrl = $uri_Clone->toString(array('scheme', 'user', 'pass', 'host', 'port', 'path', 'query', 'fragment'));
+
+					break ;
+				case 'categorys[]' :
+					$uri_Clone = clone $uri ;
+					$categorys = $uri->getVar('categorys') ;
+					$categorys[] = $value ;
+					$uri_Clone->setVar('categorys' , $categorys ) ;
+					# Создаем Ссылку
+					$currentUrl = $uri_Clone->toString(array('scheme', 'user', 'pass', 'host', 'port', 'path', 'query', 'fragment'));
+					break ;
+				default :
+
+					$nameExplode = explode('[' , $name ) ;
+					$varName =  $nameExplode[0] ;
+					$uri_Clone = clone $uri ;
+ 					$Var = $characteristics_Var ;
+
+					foreach( $nameExplode as $item )
+					{
+						$item = preg_replace("/[^0-9]/", "",$item) ;
+						if( !$item ) continue ; #END IF
+						$Var[$item][] = $value ;
+						$uri_Clone->setVar( $varName , $Var ) ;
+						unset( $Var ) ;
+						$uri = $uri_Clone ;
+
+					}#END FOREACH
+					# Создаем Ссылку
+					$currentUrl = $uri_Clone->toString(array('scheme', 'user', 'pass', 'host', 'port', 'path', 'query', 'fragment'));
+			}
+			# Клонируем елемент
+			$new_A_clone = $new_A->cloneNode();
+			$new_A_clone->setAttribute('href' , $currentUrl ) ;
+			$labelElements_clone = $labelElements[0]->childNodes[0]->cloneNode(true);
+			$labelElements[0]->childNodes[0]->parentNode->replaceChild( $new_A_clone,$labelElements[0]->childNodes[0]  );
+			$new_A_clone->appendChild( $labelElements_clone ) ;
+		}
+		$body =  $dom->saveHTML() ;
+		$this->app->setBody( $body ) ;
 	}
 
 	/**
